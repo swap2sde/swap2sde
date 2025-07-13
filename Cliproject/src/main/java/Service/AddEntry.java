d02f646e203b1c2bd0f2830345c04323680acbca
swap2sde <swap2sde@gmail.com>
2025-07-13 19:36:28 +0530

    commiting

--- Cliproject/src/main/java/Service/AddEntry.java
+++ Cliproject/src/main/java/Service/AddEntry.java
@@ -12,105 +12,105 @@ import java.util.Scanner;
 
 public class AddEntry {
 
-   public static void addNewEntry(){
+    public static void addNewEntry(){
 
-       // Share the sample How to make entry
-       // Take the new entry
-       // data will store in DB
+        // Share the sample How to make entry
+        // Take the new entry
+        // data will store in DB
 
 
         // Share the sample How to make entry
-       /**
-        * {
-        * {
-        *     Name:
-        *     Address:
-        *     Task:
-        *     Severity:
-        *     Status:
-        * }
-        * UniqueNumber
-        * }
-        */
-       System.out.println();
-       System.out.println("Provide entry in below format or type Exit to escape");
-       System.out.println("{\"Name\":\"\",\"Address\":\"\",\"Task\":\"\",\"Severity\":\"\",\"Status\":\"\"}");
+        /**
+         * {
+         * {
+         *     Name:
+         *     Address:
+         *     Task:
+         *     Severity:
+         *     Status:
+         * }
+         * UniqueNumber
+         * }
+         */
+        System.out.println();
+        System.out.println("Provide entry in below format or type Exit to escape");
+        System.out.println("{\"Name\":\"\",\"Address\":\"\",\"Task\":\"\",\"Severity\":\"\",\"Status\":\"\"}");
 
-       System.out.println();
-       System.out.print("Make your entry : " );
-       // Take the new entry
+        System.out.println();
+        System.out.print("Make your entry : " );
+        // Take the new entry
 
-       try {
-           Scanner sn = new Scanner(System.in);
+        try {
+            Scanner sn = new Scanner(System.in);
 
-           String Entry = sn.nextLine();
+            String Entry = sn.nextLine();
 
-           if(Entry.equalsIgnoreCase("Exit")){
-               MainClass.run();
-           }
-           else{
-               System.out.println();
+            if(Entry.equalsIgnoreCase("Exit")){
+                MainClass.run();
+            }
+            else{
+                System.out.println();
 
-               JSONObject obj = new JSONObject(Entry.trim());
+                JSONObject obj = new JSONObject(Entry.trim());
 
-               System.out.println("Printing obj: " + obj);
+                System.out.println("Printing obj: " + obj);
 
-               System.out.println("Get Name :" + obj.getString("Name"));
-               System.out.println("Get Address :" + obj.getString("Address"));
-               System.out.println("Get Task :" + obj.getString("Task"));
-               System.out.println("Get Severity :" + obj.getString("Severity"));
-               System.out.println("Get Status :" + obj.getString("Status"));
-
-
-               String Name = obj.getString("Name");
-               String Address = obj.getString("Address");
-               String Task = obj.getString("Task");
-               String Severity = obj.getString("Severity");
-               String Status = obj.getString("Status");
+                System.out.println("Get Name :" + obj.getString("Name"));
+                System.out.println("Get Address :" + obj.getString("Address"));
+                System.out.println("Get Task :" + obj.getString("Task"));
+                System.out.println("Get Severity :" + obj.getString("Severity"));
+                System.out.println("Get Status :" + obj.getString("Status"));
 
 
-               addDatatoDB(Name,Address,Task,Severity,Status);
+                String Name = obj.getString("Name");
+                String Address = obj.getString("Address");
+                String Task = obj.getString("Task");
+                String Severity = obj.getString("Severity");
+                String Status = obj.getString("Status");
 
-           }
-       }
-       catch (Exception e) {
-          // System.out.println("RunTime :"+ e);
-           System.out.println("--Entry is not correct please try again--"+ e);
-           addNewEntry();
-       }
+
+                addDatatoDB(Name,Address,Task,Severity,Status);
+
+            }
+        }
+        catch (Exception e) {
+            // System.out.println("RunTime :"+ e);
+            System.out.println("--Entry is not correct please try again--"+ e);
+            addNewEntry();
+        }
 
     }
 
     // data will store in DB
-    public static String addDatatoDB(String name, String address, String task, String severity, String status) {
+    public static void addDatatoDB(String name, String address, String task, String severity, String status) {
 
-       try(Connection conn = OJDBCConnecter.getConnection()){
+        try(Connection conn = OJDBCConnecter.getConnection()){
 
-           String sql = "INSERT INTO DAILYTASK (NAME,ADDRESS,TASK,SEVERITY,STATUS) VALUES(?,?,?,?,?)";
-         //  System.out.println(sql);
-           PreparedStatement stmt = conn.prepareStatement(sql);
+            String sql = "INSERT INTO DAILYTASK (NAME,ADDRESS,TASK,SEVERITY,STATUS) VALUES(?,?,?,?,?)";
+            //  System.out.println(sql);
+            PreparedStatement stmt = conn.prepareStatement(sql);
 
-           stmt.setString(1,name.toUpperCase());
-           stmt.setString(2,address.toUpperCase());
-           stmt.setString(3,task.toUpperCase());
-           stmt.setString(4,severity.toUpperCase());
-           stmt.setString(5,status.toUpperCase());
+            stmt.setString(1,name.toUpperCase());
+            stmt.setString(2,address.toUpperCase());
+            stmt.setString(3,task.toUpperCase());
+            stmt.setString(4,severity.toUpperCase());
+            stmt.setString(5,status.toUpperCase());
 
 
-           int rowsInserted = stmt.executeUpdate();
+            int rowsInserted = stmt.executeUpdate();
 
-           if (rowsInserted > 0) {
-               System.out.println();
-               System.out.println("Entry has been inserted sucessfully.");
-           }
+            if (rowsInserted > 0) {
+                System.out.println();
+                System.out.println("Entry has been inserted sucessfully.");
+            }
 
-           MainClass.run();
+            MainClass.run();
 
-       } catch (SQLException | ClassNotFoundException e) {
-           throw new RuntimeException(e);
-       }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
-        return null;
+     //   return null;
     }
 
 
--- Cliproject/src/main/java/Service/DeleteEntry.java
+++ Cliproject/src/main/java/Service/DeleteEntry.java
@@ -1,20 +1,23 @@
 package Service;
 
+import Controller.MainClass;
 import DBConnection.OJDBCConnecter;
 import org.json.JSONArray;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.*;
 import java.util.Scanner;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DeleteEntry {
 
-    public static void deleteExistingEntry(){
-                //Ask which entry he/she wants to delete HIGH,MEDIUM,LOW
-                //Show the entry according to Severity
-                //Take entry for which data to delete
-                //Take YES or NO
-                //Finally action to be performed
+    public static void deleteExistingEntry() {
+        //Ask which entry he/she wants to delete HIGH,MEDIUM,LOW
+        //Show the entry according to Severity
+        //Take entry for which data to delete
+        //Take YES or NO
+        //Finally action to be performed
 
         try {
             //Ask which entry he/she wants to delete HIGH,MEDIUM,LOW
@@ -47,7 +50,7 @@ public class DeleteEntry {
 
                 try (Connection conn = OJDBCConnecter.getConnection()) {
                     String sql = "SELECT Listagg(JSON_OBJECT(id,name,task,status),',') AS json_result FROM DAILYTASK WHERE SEVERITY =?";
-                    System.out.println("Delete query" + sql+" " + serverity);
+                    System.out.println("Delete query" + sql + " " + serverity);
                     PreparedStatement stmt = conn.prepareStatement(sql);
 
                     stmt.setString(1, serverity);
@@ -58,13 +61,20 @@ public class DeleteEntry {
                     if (rs.next()) {
                         String jsonString = rs.getString("json_result");
 
-                        if (jsonString != null && !jsonString.isEmpty()) {
+                        if (jsonString != null && !jsonString.isEmpty() && !jsonString.equals("")) {
                             // Wrap in brackets to convert to valid JSON array
-                            results = new JSONArray("[" + jsonString + "]");
+                            results = new JSONArray("[" + jsonString +  "]");
+                        }
+                        else{
+                            System.out.println("**Entry doesn't exist in " +  serverity + " catgeory**");
+                            System.out.println();
+                            deleteExistingEntry();
                         }
                     }
                     System.out.println(results.toString());
+                    System.out.println();
 
+                    rerun();
 
                 } catch (Exception e) {
                     System.out.println("Please the error while deleting" + e);
@@ -72,11 +82,108 @@ public class DeleteEntry {
                 }
             }
         } catch (Exception e) {
-            System.out.println("--Entry is not correct please try again--"+ e);
+            System.out.println("--Entry is not correct please try again--" + e);
             deleteExistingEntry();
         }
 
 
+    }
+
+    public static void deleteEntryHere() throws SQLException, ClassNotFoundException {
+
+        Scanner sn = new Scanner(System.in);
+
+        System.out.print("Enter the ID : ");
+
+        int num = sn.nextInt();
+
+        //     CheckEntryExist(num);
+
+        if(CheckEntryExist(num).equals("true")) {
+            try (Connection conn = OJDBCConnecter.getConnection()) {
+                String sql = "DELETE FROM DAILYTASK WHERE ID = ?";
+                PreparedStatement stmnt = conn.prepareStatement(sql);
+                stmnt.setInt(1, num);
+
+                System.out.print("1. Are you sure you want to delete the entry entry Y/N: ");
+                String a = sn.next();
+
+                if (a.equals("Y")) {
+                    int rs = stmnt.executeUpdate();
+                    if (rs > 0) {
+                        System.out.println("Entry deleted successfully.");
+                    }
+                } else if (a.equals("N")) {
+                    rerun();
+                } else {
+                    System.out.println("Please make a valid entry");
+                    rerun();
+                }
+
+            } catch (Exception e) {
+
+            }
+        }
+        else {
+            System.out.println("**** Entry does not exist with the given ID, Kindly enter a valid input****");
+            rerun();
+        }
+    }
+
+    public static void rerun() throws SQLException, ClassNotFoundException {
+
+
+        System.out.println("1. Want rerun");
+        System.out.println("2. Delete entry");
+        System.out.println("3. Go back to main menu");
+
+        System.out.print("Your input : "  );
+
+        Scanner sn = new Scanner(System.in);
+        int del = sn.nextInt();
+
+        if(del == 1){
+            deleteExistingEntry();
+        }
+        else if(del == 2){
+            deleteEntryHere();
+        }
+        else if (del == 3){
+            MainClass.run();
+        }
+        else {
+            System.out.println("Please provide a valid entry ");
+            rerun();
+        }
+
+
+    }
+
+    public static String CheckEntryExist(int num){
+        String bol = "false";
+
+        try(Connection conn = OJDBCConnecter.getConnection()) {
+            String sql = "SELECT COUNT(*) AS COUNT FROM DAILYTASK WHERE ID = ?";
+            PreparedStatement stmnt = conn.prepareStatement(sql);
+            stmnt.setInt(1,num);
+            ResultSet rs = stmnt.executeQuery();
+
+
+
+            if(rs.next()){
+                int count = rs.getInt("COUNT");
+                if (count >0) {
+                    System.out.println("count :" + count);
+                    bol = "true";
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println("While checking Entry Exist CheckEntryExist : " + e);
+        }
+
+        System.out.println("bol : "+ bol);
+        return bol;
     }
 
 }
